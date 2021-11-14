4bee1e2808d7ac6449ee6062bb76827d59bbf199
GaboParraG <gabrieldavidparrag@gmail.com>
2021-11-13 20:55:28 -0500

    login

--- app/src/main/java/com/gabriel/tumedicina/ui/Loguin.kt
+++ app/src/main/java/com/gabriel/tumedicina/ui/Loguin.kt
@@ -1,11 +1,12 @@
-package com.gabriel.tumedicina
+package com.gabriel.tumedicina.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gabriel.tumedicina.R
 
-class MainActivity : AppCompatActivity() {
+class Loguin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_loguin)
     }
 }
--- app/src/main/java/com/gabriel/tumedicina/ui/MainActivity.kt
+++ app/src/main/java/com/gabriel/tumedicina/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.gabriel.tumedicina
+package com.gabriel.tumedicina.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gabriel.tumedicina.R
 
-class Loguin : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loguin)
+        setContentView(R.layout.activity_main)
+
+
     }
 }
