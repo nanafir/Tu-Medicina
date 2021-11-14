5d5edd04f5b483de9e3b2a2ab260c18b94597a60
GaboParraG <gabrieldavidparrag@gmail.com>
2021-11-13 20:37:07 -0500

    login

---
+++ app/src/main/java/com/gabriel/tumedicina/Loguin.kt
@@ -0,0 +1,11 @@
+package com.gabriel.tumedicina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Loguin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loguin)
+    }
+}
