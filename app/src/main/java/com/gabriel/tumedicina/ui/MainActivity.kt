41ccca7ee10f570add947b40a282e0f23811af4a
GaboParraG <gabrieldavidparrag@gmail.com>
2021-11-15 14:12:40 -0500

    navigation

--- app/src/main/java/com/gabriel/tumedicina/ui/MainActivity.kt
+++ app/src/main/java/com/gabriel/tumedicina/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gabriel.tumedicina.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.gabriel.tumedicina.R
 
 class MainActivity : AppCompatActivity() {
